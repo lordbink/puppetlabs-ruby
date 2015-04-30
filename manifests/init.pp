38ab8c39852001198cfa4784cf1cdc248308fdde
rich.anderson <rich.anderson@Mbx.com>
2015-04-30 14:52:11 -0500

    added gem 2.4, untested

--- manifests/init.pp
+++ manifests/init.pp
@@ -228,6 +228,12 @@ class ruby (
                 warning('No binary for Ruby 2.1.x available from default repositories')
               }
             }
+            /^2\.2.*$/:{
+              $real_default_bin  = "${ruby::params::ruby_bin_base}2.2"
+              if ! $suppress_warnings {
+                warning('No binary for Ruby 2.2.x available from default repositories')
+              }
+            }
             default: {
               fail('Unable to resolve default ruby binary')
             }
@@ -256,6 +262,24 @@ class ruby (
               $real_default_gem  = "${ruby::params::ruby_gem_base}2.1"
               if ! $suppress_warnings {
                 warning('No binary package for Ruby 2.1.x available from default repositories')
+              }
+            }
+            /^2\.2.*$/:{
+              $real_default_gem  = "${ruby::params::ruby_gem_base}2.2"
+              if ! $suppress_warnings {
+                warning('No binary package for Ruby 2.2.x available from default repositories')
+              }
+            }
+            /^2\.3.*$/:{
+              $real_default_gem  = "${ruby::params::ruby_gem_base}2.3"
+              if ! $suppress_warnings {
+                warning('No binary package for Ruby 2.3.x available from default repositories')
+              }
+            }
+            /^2\.4.*$/:{
+              $real_default_gem  = "${ruby::params::ruby_gem_base}2.4"
+              if ! $suppress_warnings {
+                warning('No binary package for Ruby 2.4.x available from default repositories')
               }
             }
             default: {
