4536ab9c796dcde2eb8138e6434de1eeed74948d
rich.anderson <rich.anderson@Mbx.com>
2015-04-30 14:48:16 -0500

    removed extra lines.

--- manifests/init.pp
+++ manifests/init.pp
@@ -141,11 +141,6 @@ class ruby (
                 }
               }
             /^2\.2.*$/:{
-                $real_ruby_package  = "${ruby::params::ruby_package}2.2"
-                if ! $suppress_warnings {
-                  warning('Packages for Ruby 2.2 are not available from default repositories.')
-              }
-            /^2\.2.*$/:{
               $real_ruby_package  = "${ruby::params::ruby_package}2.2"
               if ! $suppress_warnings {
                 warning('Packages for Ruby 2.2 are not available from default repositories.')
