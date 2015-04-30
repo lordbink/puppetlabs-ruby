446b7658e0a8da59141ea82bcff9dec0b368fb21
rich.anderson <rich.anderson@Mbx.com>
2015-04-30 14:12:19 -0500

    Added compatibility with ruby 2.2.*

--- manifests/init.pp
+++ manifests/init.pp
@@ -139,6 +139,11 @@ class ruby (
               if ! $suppress_warnings {
                 warning('Packages for Ruby 2.1 are not available from default repositories.')
               }
+            /^2\.2.*$/:{
+                $real_ruby_package  = "${ruby::params::ruby_package}2.2"
+                if ! $suppress_warnings {
+                  warning('Packages for Ruby 2.2 are not available from default repositories.')
+              }
             }
             default: {
               $real_ruby_package  = $ruby_package
